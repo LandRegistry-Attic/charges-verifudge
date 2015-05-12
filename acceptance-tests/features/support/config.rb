18d46aee283af253881d6986be20dd0afd6baa95
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-05-12 08:54:06 +0100

    added comments explaining some of the files setup files and scripts

--- acceptance-tests/features/support/config.rb
+++ acceptance-tests/features/support/config.rb
@@ -1,4 +1,9 @@
-### Configure Capybara and Poltergeist
+#################################################################################################
+### This file contains details of any setup of libraries/gems and such that will be needed    ###
+### for creating and running the acceptance tests.                                            ###
+#################################################################################################
+
+### Configure Capybara and Poltergeist for running tests against web applications
 require 'capybara/cucumber'
 require 'capybara/poltergeist'
 
@@ -14,6 +19,6 @@ Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, :inspector => true, :js_errors => true)
 end
 
-### Configure Assertions
+### Configure Assertions so we can add assertions like assert_match() in our acceptance tests
 require 'test/unit'
 include Test::Unit::Assertions
--- acceptance-tests/run_tests.sh
+++ acceptance-tests/run_tests.sh
@@ -1,17 +1,25 @@
+
+#################################################################################################
+### This script is used to run any acceptance tests in the repository. It saves time and      ###
+### confusion of executing any setup before the tests are run by including it all in one      ###
+### handy script.                                                                             ###
+#################################################################################################
+
 #!/bin/bash
 
 set -e
 
-#Remove any error screenshots from previous runs
+### Remove any test failure screenshots from previous runs.
 rm -f sshot*
 
+### Installs any gems specified in the gemfile.
 bundle install
 
 if [ -z "$1"]
   then
-  #If no arguments supplied
+  ### If no arguments supplied when running this script then execute the following code.
   cucumber --tags ~@wip
 else
-  #If arguments supplied
+  ### If arguments are supplied when running this script then depending on the arguments execute the following code.
   cucumber $1
 fi
