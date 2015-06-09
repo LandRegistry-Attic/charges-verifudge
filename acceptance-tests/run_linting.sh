ee9e73f265ea1e40c1220f09bdb21233172b91df
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-06-09 11:37:43 +0100

    propogating rubocop PR check changes

--- acceptance-tests/run_linting.sh
+++ acceptance-tests/run_linting.sh
@@ -3,5 +3,17 @@
 ### folder such as Rubocop.                                                                   ###
 #################################################################################################
 
+### Retrieve the current location the script is running
+currentLocation="$(cd "$(dirname "$0")"; pwd)"
+
+### Set the location of the Gemfile based on the scripts current running location
+gemfile=$currentLocation/Gemfile
+
+### Installs any gems specified in the gemfile.
+BUNDLE_GEMFILE=$gemfile bundle install
+
 ### Run rubocop gem to check acceptance tests against the Ruby style guide.
-rubocop -c rubocop.yml
+BUNDLE_GEMFILE=$gemfile bundle exec rubocop -c $currentLocation/rubocop.yml
+
+### Return from script with exit code
+exit $?
