0d4805cf9f9a902e7e0ddcde253adb87a659b109
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-06-09 11:39:38 +0100

    propogating test setup changes for running in integration

--- acceptance-tests/run_tests.sh
+++ acceptance-tests/run_tests.sh
@@ -6,19 +6,25 @@
 
 #!/bin/bash
 
+### Retrieve the current location the script is running
+currentLocation="$(cd "$(dirname "$0")"; pwd)"
+
+### Set the location of the Gemfile based on the scripts current running location
+gemfile=$currentLocation/Gemfile
+
 set -e
 
 ### Remove any test failure screenshots from previous runs.
 rm -f sshot*
 
 ### Installs any gems specified in the gemfile.
-bundle install
+BUNDLE_GEMFILE=$gemfile bundle install
 
 if [ -z "$1"]
   then
-  ### If no arguments supplied when running this script then execute the following code.
-  cucumber --tags ~@wip
+  ### If no arguments supplied when running this script then execute all tests other than those tagged as wip
+  BUNDLE_GEMFILE=$gemfile bundle exec cucumber $currentLocation --tags ~@wip
 else
-  ### If arguments are supplied when running this script then depending on the arguments execute the following code.
-  cucumber $1
+  ### If arguments are supplied when running this script then execute th tests tagged with argument specified
+  BUNDLE_GEMFILE=$gemfile bundle exec cucumber $currentLocation $1
 fi
